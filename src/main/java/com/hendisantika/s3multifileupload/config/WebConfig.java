fbe227e510863f01ed0eeb942d16c82d401b6ef4
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-11 11:13:58 +0700

    #5 - Add WebConfig

---
+++ src/main/java/com/hendisantika/s3multifileupload/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.s3multifileupload.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : s3-multi-file-upload
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/10/20
+ * Time: 11.13
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
